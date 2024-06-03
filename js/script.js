9d95260755c99263ccfbbf8a870f389d752f9366
fr0st-iwnl <119555371+fr0st-iwnl@users.noreply.github.com>
2024-06-03 08:12:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// preloader script
+    
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    setTimeout(function() {
+        loader.style.display = "none";
+        document.body.classList.add('scroll-enable');
+        }, 0);
+  
+  
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('visual-toggle');
+
+    // Function to apply the stored mode preference
+    function applyModePreference() {
+        const mode = localStorage.getItem('mode');
+        if (mode === 'light') {
+            toggle.checked = true;
+            document.body.classList.add('lightcolors');
+            document.getElementById('visual-toggle-button').classList.add('lightmode');
+        } else {
+            toggle.checked = false;
+            document.body.classList.remove('lightcolors');
+            document.getElementById('visual-toggle-button').classList.remove('lightmode');
+        }
+    }
+
+    // Call the function to apply the mode preference on page load
+    applyModePreference();
+
+
+    toggle.addEventListener('change', function() {
+        if (toggle.checked) {
+            localStorage.setItem('mode', 'light');
+            document.body.classList.add('lightcolors');
+            document.getElementById('visual-toggle-button').classList.add('lightmode');
+        } else {
+            localStorage.setItem('mode', 'dark');
+            document.body.classList.remove('lightcolors');
+            document.getElementById('visual-toggle-button').classList.remove('lightmode');
+        }
+    });
+});
+
+document.querySelector('.logo a').addEventListener('contextmenu', function(e) {
+    e.preventDefault(); // prevent user to right click on the image
+});
+
+
+
+ // Get the current year
+ const currentYear = new Date().getFullYear();
+    
+ // Update the footer with the current year
+ document.getElementById('currentYear').textContent = currentYear;
+     
